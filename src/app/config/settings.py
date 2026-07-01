b924d30c305ebd407a251b901cf1b2a0e29dd237
Vadim <wolfindeveloper@gmail.com>
2026-07-01 14:04:27 +0300

    fix REDIS_PORT

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -54,7 +54,7 @@ class Settings(BaseSettings):
 
     # Redis
     REDIS_HOST: str = "redis"
-    REDIS_PORT: int = 6381
+    REDIS_PORT: int = 6379
     REDIS_URL: str = "redis://redis:6379/1"
 
     @property
